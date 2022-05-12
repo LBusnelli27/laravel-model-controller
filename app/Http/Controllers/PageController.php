ab025f2bf9f428eb9351867da67e3bef623144fc
Busna27 <lucabusnelli2001@gmail.com>
2022-05-12 17:28:35 +0200

    feat showing result for movies

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
-        return view('movies.index');
+        $movies = Movie::all();
+
+        $data = [
+            "movie" => new Movie(),
+            "movies" => $movies,
+        ];
+
+        return view('movies.index', $data);
     }
 }
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,10 +4,21 @@
 
 @section('main-content')
     <div class="container-fluid">
+        <h1 class="text-center mb-5">Ecco tutti i film che vuoi</h1>
         <div class="row">
-            <h1 class="text-center">Ecco tutti i film che vuoi</h1>
-            <div class="col-12">
+            @foreach ($movies as $movie)
+            <div class="col-3">
+                <div class="card mb-5 border-dark" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Original title: {{ $movie->original_title }}</h6>
+                        <h6 class="card-subtitle mb-2 text-muted">Date: {{ $movie->date }}</h6>
+                        <h6 class="card-subtitle mb-4 text-muted">Nationality: {{ $movie->nationality }}</h6>
+                        <h5 class="card-subtitle mb-2">Vote: {{ $movie->vote }}</h5>
+                    </div>
+                </div>
             </div>
+            @endforeach
         </div>
     </div>
 @endsection
