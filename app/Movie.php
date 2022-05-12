26f893b21587c8b21ac87c9bb99f91e4fec01833
Busna27 <lucabusnelli2001@gmail.com>
2022-05-12 16:42:58 +0200

    refactor some things + add bootstrap

---
+++ app/Movie.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use App\Movie;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    
+}
---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title-tab', 'Index')
+
+@section('main-content')
+    <div class="container-fluid">
+        <div class="row">
+            <h1 class="text-center">Ecco tutti i film che vuoi</h1>
+            <div class="col-12">
+            </div>
+        </div>
+    </div>
+@endsection
